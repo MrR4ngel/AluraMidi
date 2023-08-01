1aef02baaf382775fd5b0c1244bfc8397b29648c
Mateus Rangel <ramos.rangel.mr@gmail.com>
2023-07-31 23:42:15 -0300

    js funcionando

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-function tocaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) { 
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`;
+
+
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    
+
+    // console.log(contador);
+}
